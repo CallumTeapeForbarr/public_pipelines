ec5080a9c8081d6de9ddaa5a76811d36af895762
cteape <callum.teape@forsyth.barr.co.nz>
2024-08-09 14:41:08 +1200

    meh

--- fb_research_pipeline.py
+++ fb_research_pipeline.py
@@ -40,7 +40,7 @@ class Pipeline:
         # This function is called when the server is started.
         # global client, embedding_function, db, reranking_function, model
         #https://docs.trychroma.com/reference/py-client
-        self.client = chromadb.HttpClient(host="localhost",port="8000")
+        self.client = chromadb.HttpClient(host="http://127.0.0.1",port="8000")
 
         #https://api.python.langchain.com/en/latest/embeddings/langchain_huggingface.embeddings.huggingface.HuggingFaceEmbeddings.html
         self.embedding_function = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL)
