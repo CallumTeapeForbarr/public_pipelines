57e31b1c26aec2ccb3864f8ba1c82db8ade40219
cteape <callum.teape@forsyth.barr.co.nz>
2024-08-27 09:45:13 +1200

    trying to increase context

--- fb_research.py
+++ fb_research.py
@@ -129,12 +129,15 @@ class Pipeline:
         }
 
 
-        return payload['messages'][1]['content']
+        # return payload['messages'][1]['content']
+
+        # api_url = 'http://ollama:11434/v1/chat/completions'
+        api_url = 'http://ollama:11434/api/generate '
 
         #https://github.com/ollama/ollama/blob/main/docs/api.md
         try:
             r = requests.post(
-                url=f"http://ollama:11434/v1/chat/completions",
+                url=api_url,
                 json=payload,
                 stream=True
             )
