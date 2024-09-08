d4720601573544285e055351835961b6daa092b8
cteape <callum.teape@forsyth.barr.co.nz>
2024-09-09 10:15:36 +1200

    pipe to ground

---
+++ pipeline_to_ground.py
@@ -0,0 +1,115 @@
+"""
+title: fb_research  
+author: callum
+description: fb research pipeline
+requirements: chromadb, sentence_transformers,requests,numpy
+"""
+
+from typing import List, Union, Generator, Iterator
+import os
+import asyncio
+import requests
+import json
+import copy
+import math
+from datetime import datetime
+import numpy as np
+
+class Pipeline:
+
+    def __init__(self):
+        self.research_collection = None
+        self.data_collection = None
+        self.time_collection = None
+        self.client = None
+        self.embedding_function = None
+        self.reranking_function = None
+        self.model = None
+        self.conversation = None
+
+
+    async def on_startup(self):
+
+        pass
+
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        pass
+
+
+    def pipe(
+        self, user_message: str, model_id: str, messages: List[dict], body: dict
+        ) -> Union[str, Generator, Iterator]:
+        # This is where you can add your custom RAG pipeline.
+        # Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response.
+
+
+        prompt = """
+                You are a helpful assistant
+            """
+
+
+        convo = copy.deepcopy(messages)
+
+        convo = convo[:-1]
+
+        convo.insert(0,
+                     {
+                         'role': 'system',
+                         'content': prompt
+                     })
+        
+        convo.append(
+                {
+                    "role": "user", 
+                    "content": user_message
+                }
+        )
+
+
+        payload = {
+            "model": "qwen2:1.5b",
+            "options": {
+                "num_ctx": 4096
+            },
+            "messages": convo,
+            "stream": body["stream"]
+        }
+
+        print(payload['messages'][1]['content'])
+
+
+        api_url = 'http://ollama:11434/api/chat'
+
+        #https://github.com/ollama/ollama/blob/main/docs/api.md
+        try:
+            r = requests.post(
+                url=api_url,
+                json=payload,
+                stream=True
+            )
+
+            r.raise_for_status()
+
+            if body["stream"]:
+                for line in r.iter_lines():
+                    if line:
+                        # Convert the line to a dictionary
+                        data = json.loads(line.decode('utf-8'))
+
+                        # Extract the content from the message
+                        if 'message' in data and 'content' in data['message']:
+                            yield data['message']['content']
+
+                        # Stop if the "done" flag is True
+                        if data.get('done', False):
+                            break
+                    else:
+                        return r.json()
+            else:
+                return r.json()
+        except Exception as e:
+            return f"Error: {e}"
+
+
