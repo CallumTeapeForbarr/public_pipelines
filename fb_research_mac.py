cd17bdd141826766a6cbf5a0819551a255d09e8e
cteape <callum.teape@forsyth.barr.co.nz>
2024-09-10 10:02:46 +1200

    he

--- fb_research_mac.py
+++ fb_research_mac.py
@@ -73,6 +73,7 @@ class Pipeline:
         # This is where you can add your custom RAG pipeline.
         # Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response.
 
+        date_string = '2024-09-01'
         company = None
         query = None
 
@@ -82,53 +83,123 @@ class Pipeline:
             query = user_message
 
         else:
-            company, query = user_message.split(';')
+            date_string, company, query = user_message.split(';')
+
+
+
+        """
+        Method to give an embedding for dates into a helical space.
+
+        I choose a helix because of the following characteristics:
+            1.  Continuous: reflects continuity of time.
+            2.  Cyclical: the shape repeats every revolution, meaning relationships within each revolution (year) are comporable
+            3.  Spacing: Can control how tightly wound the helix is to make points between years more or less similar
+        """
+        #parsing time input and choosing starting date as the first of 2020
+        date = datetime.strptime(date_string, '%Y-%m-%d')
+        the_big_bang = datetime(2020,1,1)
+
+        #choosing constants (r being radius, compression being the amount the curve travels in the z axis per revolution/how tightly wound it is)
+        r = 1
+        compression = math.sqrt((16/3))
+
+        #figuring out the number of revolutions (as a function of years since the starting date)
+        z = (date - the_big_bang).days
+        revs = z/365
+
+
+        # Computing the 3D vector:
+        # - x and y are on the circle, determined by the angle (2 * π * revs)
+        # - z is determined by the number of revolutions multiplied by the compression factor
+        vec = [[r*math.cos(2*math.pi*revs), r*math.sin(2*math.pi*revs),  compression*revs]]
 
 
         embedded_query=self.embedding_function.encode([query])
 
-
-        #if a company is provided
-        if not company is None:
-            docs = self.research_collection.query(
-                query_embeddings=embedded_query,
-                include=["documents","distances","metadatas"],
-                where = {'company': company},
-                n_results=15
-            )
-
-        #if not
-        else:
-            docs = self.research_collection.query(
-                query_embeddings=embedded_query,
-                include=["documents","distances","metadatas"],
-                n_results=15
-            )
-
-        # context = '\n'.join(docs['documents'][0])
-
+            #searching the space of sentence transformer embedded text
+        # research_collection = self.client.get_or_create_collection(name="research")
+        text_rankings = self.research_collection.query(
+            query_embeddings=embedded_query,
+            include=["documents","distances","metadatas"],
+            where = {'company': company},
+            n_results=int(self.research_collection.count()/5))   #trying to keep this relatively small while retaining enough as to not throw away the time relevant points
         
-        reranked = self.reranking_function.rank(
-            query,
-            docs["documents"][0],
-            top_k=5,
-            return_documents=True
-        )
+        #searching the space of helix embedded dates
+        # time_collection = client.get_or_create_collection(name='time')
+        time_rankings = self.time_collection.query(
+            query_embeddings=vec,
+            include=["documents","distances","metadatas"],
+            where = {'company': company},
+            n_results=self.time_collection.count())  #everything (assuming this search won't take too long as it's a 3d space)
+
+
+        #creating normalised dictionary of time collection rankings
+        time_distances = np.array(time_rankings['distances'][0])
+        time_distances_normalised = time_distances/np.max(time_distances)
+        time_dict = dict(zip(time_rankings['ids'][0], time_distances_normalised))
+
+        #creating normalised dictionary of text collection rankings
+        text_distances = np.array(text_rankings['distances'][0])
+        text_distances_normalised = text_distances/np.max(text_distances)
+        text_dict = dict(zip(text_rankings['ids'][0],text_distances_normalised))
+
+
+        #combining the two rankings together
+        combined = {}
+        #iterate over text_dict keys, note that time_dict has every id in the database, so this should never throw a key error
+        for key in text_dict.keys():
+                combined[key] = text_dict[key] + time_dict[key]   #equal parts each
+
+
+        #reordering the combined dcitionary to sort by the lowest distance
+        ranking = dict(sorted(combined.items(), key=lambda item: item[1]))   
+        # print(ranking)
+
+        sources = []
+        # docs = []
+        # dates = []
 
         context = ''
-        sources = []
 
-        for ranking in reranked:
-            context += docs['metadatas'][0][ranking['corpus_id']]['date']
-            context += '\n'
-            context += ranking['text']
-            context += '\n\n'
+        #now taking the top ten of the combined ranking (source, publication date, and text itself)
+        for id in list(ranking.keys())[:10]:
+            document = self.research_collection.get(ids=[id])
 
-            sources.append(docs['metadatas'][0][ranking['corpus_id']]['source'].split('/')[-1].split('.')[0])
+            source_str = document['metadatas'][0]['source']
+            date_str = document['metadatas'][0]['date']
+            document_str = document['documents'][0]
 
+            context += f'{date_str}\n{document_str}\n'
+
+            sources.append(source_str)
+            # dates.append(date_str)
+            # docs.append(document_str)
 
         deduped_sources = list(set(sources))
         source_text = '\n'.join(deduped_sources)
+
+        
+        # reranked = self.reranking_function.rank(
+        #     query,
+        #     docs["documents"][0],
+        #     top_k=5,
+        #     return_documents=True
+        # )
+
+        # context = ''
+        # sources = []
+
+        # for ranking in reranked:
+        #     context += docs['metadatas'][0][ranking['corpus_id']]['date']
+        #     context += '\n'
+        #     context += ranking['text']
+        #     context += '\n\n'
+
+        #     sources.append(docs['metadatas'][0][ranking['corpus_id']]['source'].split('/')[-1].split('.')[0])
+
+
+        # deduped_sources = list(set(sources))
+        # source_text = '\n'.join(deduped_sources)
         
         try:
             data = self.data_collection.query(
@@ -216,7 +287,7 @@ class Pipeline:
 
                         # Stop if the "done" flag is True
                         if data.get('done', False):
-                            yield(f"\n\n\n\n{source_text}")
+                            yield(f"\n{source_text}")
                             break
                     else:
                         return r.json()
